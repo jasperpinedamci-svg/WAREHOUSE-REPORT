127f9c66c6881e7dd65aeb30b21e0fd3d509111d
Jas <jasperpineda.mci@gmail.com>
2026-05-19 10:15:59 +0800

    Update LandedCost.sql

--- LandedCost.sql
+++ LandedCost.sql
@@ -4,7 +4,8 @@ SELECT
     L0."DocNum" AS "Last Landed Cost No.",
     L0."DocDate" AS "Last Landed Cost Date",
     COALESCE(L0."PriceAtWH", 0) AS "Last Landed Cost",
-    I0."AvgPrice" AS "SAP Average Cost"
+    I0."AvgPrice" AS "SAP Average Cost",
+    (select MAX("DocDate") from oinm where "ItemCode" = I0."ItemCode" and "TransType" = '20') "Lst Rcpt Date"
 
 FROM OITM I0
 LEFT JOIN (
@@ -16,11 +17,13 @@ LEFT JOIN (
     FROM IPF1 A0
     INNER JOIN OIPF A1 ON A0."DocEntry" = A1."DocEntry"
     WHERE A1."Canceled" = 'N'
-      AND A0."DocEntry" = (SELECT MAX(B0."DocEntry") 
-                           FROM IPF1 B0 INNER JOIN OIPF B1 ON B0."DocEntry" = B1."DocEntry"
-                           WHERE B1."Canceled" = 'N' 
-                           AND B0."ItemCode" = A0."ItemCode")
-    ) L0 ON I0."ItemCode" = L0."ItemCode"
-
+      AND A0."DocEntry" = (
+          SELECT MAX(B0."DocEntry") 
+          FROM IPF1 B0
+          INNER JOIN OIPF B1 ON B0."DocEntry" = B1."DocEntry"
+          WHERE B1."Canceled" = 'N' 
+            AND B0."ItemCode" = A0."ItemCode"
+      )
+) L0 ON I0."ItemCode" = L0."ItemCode"
 WHERE I0."InvntItem" = 'Y' AND I0."ItmsGrpCod" = '156'
-ORDER BY I0."ItemCode"
+ORDER BY I0."ItemCode";
