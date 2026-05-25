8e99d27bea33be8c7ea3100d6acad521efdb1523
Jas <jasperpineda.mci@gmail.com>
2026-05-25 14:37:31 +0800

    Update DRCancellationMonitoring.sql

--- DRCancellationMonitoring.sql
+++ DRCancellationMonitoring.sql
@@ -14,11 +14,13 @@ UNION ALL
 
 SELECT DISTINCT
     'CM' AS "Cancellation Type",
-    T2."DocNum" AS "Orig DR. No",
+    T3."DocNum" AS "Orig DR. No",
     T0."DocNum" AS "Cancellation Doc. No.",
     T0."DocDate" AS "Cancellation Date",
     T0."Comments" AS "Remarks"
 FROM ORIN T0
 INNER JOIN RIN1 T1 ON T0."DocEntry" = T1."DocEntry"
-INNER JOIN ODLN T2 ON T1."BaseEntry" = T2."DocEntry"
-WHERE T1."BaseType" = 13;
+-- Bridge 1: Credit Memo to A/R Invoice
+INNER JOIN INV1 T2 ON T1."BaseEntry" = T2."DocEntry" AND T1."BaseLine" = T2."LineNum" AND T1."BaseType" = 13 
+-- Bridge 2: A/R Invoice to Delivery
+INNER JOIN ODLN T3 ON T2."BaseEntry" = T3."DocEntry" AND T2."BaseType" = 15;
